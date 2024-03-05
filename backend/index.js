90ca9722b95445af305b756723811baa0fa1c8ec
iamdarwinsai <darushyam143@gmail.com>
2024-03-05 17:31:16 +0530

    db integrated to backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
-
+dotenv.config();
+mongoose
+  .connect(process.env.MONOGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
